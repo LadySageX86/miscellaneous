a4e0ae1c9f65df051846a082d2a72b9f3b52179a
Spencer Smith <48563701+RevSpennicus@users.noreply.github.com>
2019-10-02 16:01:58 -0300

    Create fizzbuzz.cpp
    The classic Fizzbuzz exercise in C++

---
+++ fizzbuzz.cpp
@@ -0,0 +1,47 @@
+/************************
+*     FIZZBUZZ-C++      *
+*   By Spencer Smith    *
+* linkhylian8@gmail.com *
+************************/
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    int x = 1;
+    array<int,100> numbers;
+
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        numbers[i] = x;
+        x++;
+    }
+
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] % 3 == 0)
+        {
+            if (numbers[i] % 5 == 0)
+            {
+                cout << "FizzBuzz\n";
+                continue;
+            }
+            cout << "Fizz\n";
+            continue;
+        }
+        else if (numbers[i] % 5 == 0)
+        {
+            cout << "Buzz\n";
+            continue;
+        } 
+        else
+        {
+            cout << numbers[i] << endl;
+        }
+    }
+    
+    return 0;
+}
