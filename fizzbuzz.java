e843742960e04be2adef306e8d8bd2a16e977e55
Spencer Smith <48563701+RevSpennicus@users.noreply.github.com>
2019-10-02 16:00:17 -0300

    Create fizzbuzz.java
    The classic Fizzbuzz exercise in Java.

---
+++ fizzbuzz.java
@@ -0,0 +1,43 @@
+/************************
+*     FIZZBUZZ-JAVA     *
+*   By Spencer Smith    *
+* linkhylian8@gmail.com *
+************************/
+
+class Fizzbuzz
+{
+    public static void main(String[] args)
+    {
+        int x = 1;
+        int[] numbers = new int[100];
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            numbers[i] = x;
+            x++;
+        }
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            if (numbers[i] % 3 == 0)
+            {
+                if (numbers[i] % 5 == 0)
+                {
+                    System.out.println("FizzBuzz");
+                    continue;
+                }
+                System.out.println("Fizz");
+                continue;
+            }
+            else if (numbers[i] % 5 == 0)
+            {
+                System.out.println("Buzz");
+                continue;
+            } 
+            else
+            {
+                System.out.println(numbers[i]);
+            }
+        }
+    }
+}
