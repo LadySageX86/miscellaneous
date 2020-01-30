d1b85a2edd64de116f323276d0ffc29aade2fc20
Spencer Smith <48563701+RevSpennicus@users.noreply.github.com>
2020-01-30 08:07:03 -0400

    added discount_calc.c

---
+++ discount_calc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    float discountMultiplier = 1 - (strtod(argv[1], NULL)/100);
+    float amountDiscounted;
+ 
+    while (1 == 1)
+    {
+        printf("\nEnter the amount you are discounting: ");
+        scanf("%f", &amountDiscounted);
+
+        float total = amountDiscounted * discountMultiplier;
+        printf("\nTotal discount: %.2f", total);
+    }    
+    return 0;
+}
